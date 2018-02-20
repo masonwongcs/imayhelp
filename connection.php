f9d632cf0d534d0d9f7de996c90546eaa8db0d27
masonwongcs <30920904+masonwongcs@users.noreply.github.com>
2018-02-20 16:08:19 +0800

    Added connection.php

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php 
+//connection to mySQL
+$link = mysqli_connect('sql213.epizy.com', 'epiz_21475618', 'zKBsXEXSGcj2', 'epiz_21475618_services')
+        or die("Could not connect");
+$db = mysqli_select_db($link,"epiz_21475618_services")or die("Could not connect");
+?>
