e957c5c75a2cf474f63e96f8c1433ccd0c53820e
Mason Wong <Seong6232@gmail.com>
2018-02-20 10:33:39 +0800

    Update profile php

--- profile.php
+++ profile.php
@@ -11,15 +11,38 @@
 <div class="container">
     <div class="wrapper">
         <?php include 'header.php';?>
+        <?php
+          if (isset($_SESSION['email'])) {
+            $email = $_SESSION['email'];
+            $queryEmail = "SELECT * FROM `register_user` WHERE email = '$email'";
+            $resultEmail = mysqli_query($link, $queryEmail);
+            while($rowEmail = mysqli_fetch_array($resultEmail, MYSQLI_NUM))
+            {
+              $currentUserId = $rowEmail[0];
+              $firstName = $rowEmail[1];
+              $lastName = $rowEmail[2];
+              $email = $rowEmail[3];
+              $pwd = $rowEmail[4];
+              $mobileNo = $rowEmail[5];
+              $address = $rowEmail[6];
+              $country = $rowEmail[7];
+              $gender = $rowEmail[8];
+              $acc_SSM = $rowEmail[9];
+              $company_name = $rowEmail[10];
+              $service = $rowEmail[11];
+
+            }
+          }
+        ?>
         <div class="register-form-wrapper">
           <div class="ui segment" data-tab="register">
               <form class="ui form" action="register.php" method="post">
-                  <h4 class="ui dividing header">User Information</h4>
+                  <h4 class="ui dividing header">Update User Information</h4>
                   <div class="field">
                       <label>Name</label>
                       <div class="two fields">
                           <div class="field">
-                              <input type="text" name="firstname" placeholder="First Name">
+                              <input type="text" name="firstname" placeholder="First Name" value="<?php echo $firstName;?>">
                           </div>
                           <div class="field">
                               <input type="text" name="lastname" placeholder="Last Name">
