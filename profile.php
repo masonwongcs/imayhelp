03ca88dd12ae90372612b3b1db695078bf5a9fcc
Mason Wong <Seong6232@gmail.com>
2018-02-20 10:24:35 +0800

    Updated profile.php

--- profile.php
+++ profile.php
@@ -5,121 +5,111 @@
     <link rel="stylesheet" href="css/semantic.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/slider.css">
-    <title>IHELPYOU</title>
+    <title>Profile</title>
 </head>
 <body>
- <div class="container">
+<div class="container">
     <div class="wrapper">
         <?php include 'header.php';?>
-
-        <?php
-            include ('connection.php');
-
-            if (isset($_GET['serviceId'])) {
-
-                $serviceId = $_GET['serviceId'];
-                if($serviceId == ""){
-                    echo "
-                        <script type=\"text/javascript\">
-                            location.href = \"error.php\";
-                        </script>
-                    ";
-                }
-                $query = "SELECT * FROM `post` WHERE services_id = '$serviceId'";
-                $result = mysqli_query($link, $query);
-                while($row = mysqli_fetch_array($result, MYSQLI_NUM))
-                {
-                  $serviceId = $row[0];
-                  $serviceName = $row[1];
-                  $description = $row[2];
-                  $location = $row[3];
-                  $area = $row[4];
-                  $price = $row[5];
-                  $contact = $row[6];
-                  $type = $row[7];
-                  $uploadedLocation = $row[8];
-                  $userId = $row[9];
-                  $datetime = $row[10];
-                  $likes = $row[11];
-
-                    $imageLocation;
-
-                    if($uploadedLocation !== ""){
-                        $imageLocation = $uploadedLocation;
-                    } else{
-                        $imageLocation = 'img/image.png';
-                    }
-                }
-
-                //Get current user information
-                $queryUser = "SELECT * FROM `register_user` WHERE user_id = '$userId'";
-                $resultUser = mysqli_query($link, $queryUser);
-                while($rowUser = mysqli_fetch_array($resultUser, MYSQLI_NUM))
-                {
-                  // $userId = $rowUser[0];
-                  // $firstName = $rowUser[1];
-                  // $lastName = $rowUser[2];
-                  // $email = $rowUser[3];
-                  // $pwd = $rowUser[4];
-                  // $mobileNo = $rowUser[5];
-                  // $address = $rowUser[6];
-                  // $country = $rowUser[7];
-                  // $gender = $rowUser[8];
-                  // $acc_SSM = $rowUser[9];
-                  $company_name_listing = $rowUser[10];
-                  // $service = $rowUser[11];
-                } 
-
-            } else {
-                // Fallback behaviour goes here
-               echo "
-                    <script type=\"text/javascript\">
-                        location.href = \"error.php\";
-                    </script>
-                ";
-            }
-        ?>
-        <div class="ui grid">
-            <div class="eight wide column">
-                <img class="ui fluid image" src="<?php 
-                echo $imageLocation 
-                ?>">
-            </div>
-            <div class="eight wide column contact-info">
-                <div class="ui segment">
-                    <h2 class="ui header"><?php echo $serviceName ?></h2>
-                    <div class="sub header">
-                       <?php echo $description ?>
-                    </div>
-                    <div class="ui divider"></div>
-                    <div class="ui horizontal segments">
-                        <div class="ui segment">
-                            <i class="user icon"></i>
-                            <span class="content">
-                                <?php echo $company_name_listing?>
-                            </span>
+        <div class="ui bottom attached tab segment" data-tab="register">
+            <form class="ui form" action="register.php" method="post">
+                <h4 class="ui dividing header">User Information</h4>
+                <div class="field">
+                    <label>Name</label>
+                    <div class="two fields">
+                        <div class="field">
+                            <input type="text" name="firstname" placeholder="First Name">
                         </div>
-                        <div class="ui segment">
-                            <i class="tag icon"></i>
-                            <span class="content">
-                                RM<span class="price"><?php echo $price ?></span>
-                            </span>
-                        </div>
-                        <div class="ui segment">
-                            <i class="like icon"></i>
-                            <span class="content">
-                                <span class="likes"><?php echo $likes?></span>
-                            </span>
+                        <div class="field">
+                            <input type="text" name="lastname" placeholder="Last Name">
                         </div>
                     </div>
-                    <button class="fluid ui primary button call-btn"><i class="call icon"></i><span class="phone-number"><?php echo $contact ?></span></button>
-
                 </div>
-            </div>
+                <div class="field">
+                    <label>Phone Number</label>
+                    <div class="fields">
+                        <div class="sixteen wide field">
+                            <input type="tel" name="phone" placeholder="Phone Number">
+                        </div>
+                    </div>
+                </div>
+                <div class="field">
+                    <label>Address</label>
+                    <div class="fields">
+                        <div class="sixteen wide field">
+                            <input type="text" name="address" placeholder="Street Address">
+                        </div>
+                    </div>
+                </div>
+                <div class="field">
+                    <label>Location</label>
+                    <div class="ui fluid search selection dropdown">
+                        <input type="hidden" name="country">
+                        <i class="dropdown icon"></i>
+                        <div class="default text">Select Location</div>
+                        <div class="menu">
+                           <div class="item" data-value="1">Kuala Lumpur</div>
+                                <div class="item" data-value="2">Johor</div>
+                                <div class="item" data-value="3">Kedah</div>
+                                <div class="item" data-value="4">Kelantan</div>
+                                <div class="item" data-value="5">Melaka</div>
+                                <div class="item" data-value="6">Negeri Sembilan</div>
+                                <div class="item" data-value="7">Pahang</div>
+                                <div class="item" data-value="8">Penang</div>
+                                <div class="item" data-value="9">Perak</div>
+                                <div class="item" data-value="10">Perlis</div>
+                                <div class="item" data-value="11">Putrajaya</div>
+                                <div class="item" data-value="12">Selangor</div>
+                                <div class="item" data-value="13">Sabah</div>
+                                <div class="item" data-value="14">Sarawak</div>
+                                <div class="item" data-value="15">Terengganu</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="field">
+                    <label>Gender</label>
+                    <div class="ui fluid search selection dropdown">
+                        <input type="hidden" name="gender">
+                        <i class="dropdown icon"></i>
+                        <div class="default text">Select Gender</div>
+                        <div class="menu">
+                            <div class="item" data-value="male">Male</div>
+                            <div class="item" data-value="female">Female</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label>Services</label>
+                    <div class="ui fluid search selection dropdown">
+                        <input type="hidden" name="services">
+                        <i class="dropdown icon"></i>
+                        <div class="default text">Select Services</div>
+                        <div class="menu">
+                            <div class="item" data-value="IT">Agent</div>
+                            <div class="item" data-value="IT">Consultant</div>
+                            <div class="item" data-value="IT">Insturctor</div>
+                            <div class="item" data-value="IT">Home and Office</div>
+                            <div class="item" data-value="IT">Lifestyle</div>
+                            <div class="item" data-value="IT">Technician</div>
+                            <div class="item" data-value="Mechanic">More Services are coming...</div>
+                        </div>
+                    </div>
+                </div>
+                
+                <h4 class="ui dividing header">Login Information</h4>
+                    <div class="field">
+                        <input type="text" name="email" placeholder="E-mail address">
+                    </div>
+                    <div class="field">
+                        <input type="password" name="password" placeholder="Password">
+                    </div>
+                <button class="fluid ui primary button" type="submit" name="submit">Register</button>
+            </form>
         </div>
     </div>
 </div>
-            <?php include 'footer.php';?>
+<?php include 'footer.php';?>
 <script type="application/javascript" src="js/jquery-3.2.1.min.js"></script>
 <script type="application/javascript" src="js/semantic.min.js"></script>
 <script type="application/javascript" src="js/slider.min.js"></script>
