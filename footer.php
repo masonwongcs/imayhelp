c4ffea5630fab7afa4e88df1f587c5d9d4f72750
Mason Wong <Seong6232@gmail.com>
2018-02-21 10:23:30 +0800

    Styled footer

--- footer.php
+++ footer.php
@@ -1,27 +1,3 @@
-<?php
-echo'
-
-    <div class="footer">
-        <div class="container">
-            <div class="ui inverted stackable grid">
-                <div class="four wide column">
-                    <h4 class="ui inverted header">Footer</h4>
-                    <div class="ui inverted link list">
-                        <div class="item">
-                            Nav link 1
-                        </div>
-                        <div class="item">
-                            Nav link 2
-                        </div>
-                        <div class="item">
-                            Nav link 3
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
-        </div>
-    </div>
-'
-?>
+<div class="footer">
+    <p>Copyright&nbsp;&copy;&nbsp;2018. All Rights Reserved</p>
+</div>
