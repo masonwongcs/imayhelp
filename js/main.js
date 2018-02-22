144247711ed49909725fbe32cd52b26262f2da8a
Mason Wong <Seong6232@gmail.com>
2018-02-22 18:49:51 +0800

    Bug fix

--- js/main.js
+++ js/main.js
@@ -70,17 +70,13 @@ $(document).ready(function () {
 
     $('.button.delete.post').click(function(){
         var postId = $(this).data('id');
-        var deleteSuccess = false;
         $.ajax({
             url: 'deletePost.php?services_id=' + postId,
             type: 'get',
             success: function(response){
-              deleteSuccess = true;
             }
         });
-        if(deleteSuccess){
-            $(this).parents('tr').remove();
-        }
+        $(this).parents('tr').remove();
     })
 
     // THis function will toggle view (List view or Card view) / switch view mode
