851d2fbde6a4602c55ce696b10093e3605b65f24
Mason Wong <Seong6232@gmail.com>
2018-02-22 18:44:46 +0800

    Update query

--- deletePost.php
+++ deletePost.php
@@ -2,9 +2,12 @@
 include ('connection.php');
 
 if (isset($_GET['services_id'])) {
+
+	$services_id = $_GET['services_id'];
+	
 	//Delete data
-	$result1 = mysqli_query($link, "DELETE FROM likes WHERE services_id = '".$email."'" ) or die(mysqli_error($link));
-	$result1 = mysqli_query($link, "DELETE FROM post WHERE services_id = '".$email."'" ) or die(mysqli_error($link));
+	$result1 = mysqli_query($link, "DELETE FROM likes WHERE services_id = '".$services_id."'" ) or die(mysqli_error($link));
+	$result1 = mysqli_query($link, "DELETE FROM post WHERE services_id = '".$services_id."'" ) or die(mysqli_error($link));
 }
 
  mysqli_close($link); 	 
