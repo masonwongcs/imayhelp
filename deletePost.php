c416915b51860243300a9b5181f903434e5d4bf7
Mason Wong <Seong6232@gmail.com>
2018-02-23 15:03:04 +0800

    Bug fix

--- deletePost.php
+++ deletePost.php
@@ -1,17 +1,13 @@
 <?php
 include ('connection.php');
 
-//Check only admin has rights to delete
-if($_SESSION['email'] == "Admin"){
+if (isset($_GET['services_id'])) {
 
-	if (isset($_GET['services_id'])) {
-
-		$services_id = $_GET['services_id'];
-
-		//Delete data
-		$result1 = mysqli_query($link, "DELETE FROM likes WHERE services_id = '".$services_id."'" ) or die(mysqli_error($link));
-		$result1 = mysqli_query($link, "DELETE FROM post WHERE services_id = '".$services_id."'" ) or die(mysqli_error($link));
-	}
+	$services_id = $_GET['services_id'];
+	
+	//Delete data
+	$result1 = mysqli_query($link, "DELETE FROM likes WHERE services_id = '".$services_id."'" ) or die(mysqli_error($link));
+	$result1 = mysqli_query($link, "DELETE FROM post WHERE services_id = '".$services_id."'" ) or die(mysqli_error($link));
 }
 
  mysqli_close($link); 	 
